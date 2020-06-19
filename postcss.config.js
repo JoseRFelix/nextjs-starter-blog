3ff89548d7e1fbfa29e3c0de49a3a56f162015b5
Jfelix61 <jfelix61@outlook.com>
2020-06-19 03:53:22 -0400

    Upgrade: - increase versions of packages - Removed purgecss and autoprefixer - Added tailwind's proprietary purgecss integration - Added jsconfig for absolute imports

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,3 @@
-const purgecss = [
-  "@fullhuman/postcss-purgecss",
-  {
-    content: ["./components/**/*.js", "./pages/**/*.js"],
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-    whitelist: ["lazyloaded"],
-  },
-];
-
 module.exports = {
-  plugins: [
-    "postcss-import",
-    "tailwindcss",
-    "autoprefixer",
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
-  ],
+  plugins: ["tailwindcss", "postcss-preset-env"],
 };
