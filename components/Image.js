bbd96f00ca46181ad4531a047bd9626e1c3a2f4a
Jose Felix <jfelx@outlook.com>
2020-10-29 05:11:29 -0400

    update: migrate image to picture syntax

--- components/Image.js
+++ components/Image.js
@@ -2,12 +2,10 @@ import "lazysizes";
 
 export default function Image({ alt, src, previewSrc, webpSrc, className }) {
   return (
-    <img
-      className={`lazyload blur-up ${className}`}
-      alt={alt}
-      src={src}
-      srcset={previewSrc}
-      data-srcset={webpSrc}
-    />
+    <picture>
+      <source type="image/webp" data-srcset={webpSrc} />
+      <source type="image/png" data-srcset={src} />
+      <img className={`lazyload blur-up ${className}`} alt={alt} src={src} />
+    </picture>
   );
 }
